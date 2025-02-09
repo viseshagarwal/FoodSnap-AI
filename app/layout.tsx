f2e1e1bbb428e373638aa13c0cefc7a8d11ecf6c
Visesh Agarwal <viseshagarwal@outlook.com>
2025-02-09 23:38:59 +0530

    bhjbhj

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,24 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
-import ClientLayout from './ClientLayout'
-import { metadata } from './metadata'
+import "./globals.css";
+import { Inter } from "next/font/google";
+import ClientLayout from "./ClientLayout";
+import { metadata } from "./metadata";
+import { Analytics } from "@vercel/analytics/react";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
-export { metadata }
+export { metadata };
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>
         <ClientLayout>{children}</ClientLayout>
+        <Analytics />
       </body>
     </html>
-  )
+  );
 }
